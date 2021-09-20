78098327cd537e2a82e64e12542e1ce5b50af3da
ljq0067 <75825534+ljq0067@users.noreply.github.com>
2021-09-20 17:49:05 -0400

    Update Keepa_category.py

--- data gathering/API_python/Keepa/Keepa_category.py
+++ data gathering/API_python/Keepa/Keepa_category.py
@@ -1,7 +1,7 @@
 import requests
 
 
-url = f'https://api.keepa.com/category?key=7shsdsmam3tg745bicbo6eek3l0h9hf0a7i06a1otjpsb05d188kslln8h0jtsds&domain=1&category=0&parents=1'
+url = f'https://api.keepa.com/category?key=<Your Keepa API Key>&domain=1&category=0&parents=1'
 req = requests.get(url)
 with open(f'category.json', 'wb') as f:
     f.write(req.content)
