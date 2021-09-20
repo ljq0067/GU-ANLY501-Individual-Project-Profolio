59e4b174b102ca99316b0cc3a6af6f4f7a65ae12
ljq0067 <75825534+ljq0067@users.noreply.github.com>
2021-09-20 17:49:39 -0400

    Update Keepa_product.py

--- data gathering/API_python/Keepa/Keepa_product.py
+++ data gathering/API_python/Keepa/Keepa_product.py
@@ -9,7 +9,7 @@ ASIN = asinStr[241241:242241]
 print(len(ASIN))
 
 # March 19 — California Issues Statewide Stay-at-Home Order
-url = f'https://api.keepa.com/product?key=7shsdsmam3tg745bicbo6eek3l0h9hf0a7i06a1otjpsb05d188kslln8h0jtsds&domain=1&asin={ASIN}&stat=2020-03-19,2022-01-01&history=1'
+url = f'https://api.keepa.com/product?key=<Your Keepa API Key>&domain=1&asin={ASIN}&stat=2020-03-19,2022-01-01&history=1'
 req = requests.post(url)
 with open(f'product/product241.json', 'wb') as f:
     f.write(req.content)
