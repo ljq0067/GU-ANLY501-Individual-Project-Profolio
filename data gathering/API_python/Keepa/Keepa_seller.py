a416130443ecaaf951695bb3d3b6b983b39c3a63
ljq0067 <75825534+ljq0067@users.noreply.github.com>
2021-09-20 17:50:08 -0400

    Update Keepa_seller.py

--- data gathering/API_python/Keepa/Keepa_seller.py
+++ data gathering/API_python/Keepa/Keepa_seller.py
@@ -9,7 +9,7 @@ sellerId = asinStr[44253: 45728]
 print(len(sellerId))
 
 # March 19 — California Issues Statewide Stay-at-Home Order
-url = f'https://api.keepa.com/seller?key=7shsdsmam3tg745bicbo6eek3l0h9hf0a7i06a1otjpsb05d188kslln8h0jtsds&domain=1&seller={sellerId}'
+url = f'https://api.keepa.com/seller?key=<Your Keepa API Key>&domain=1&seller={sellerId}'
 req = requests.post(url)
 with open(f'seller/seller29.json', 'wb') as f:
     f.write(req.content)
