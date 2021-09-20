dd1e3968be99b29d89b94d3e7ca8df97b2dce3ca
ljq0067 <75825534+ljq0067@users.noreply.github.com>
2021-09-20 17:50:24 -0400

    Update Keepa_topseller.py

--- data gathering/API_python/Keepa/Keepa_topseller.py
+++ data gathering/API_python/Keepa/Keepa_topseller.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_data():
-    url = f'https://api.keepa.com/topseller?key=7shsdsmam3tg745bicbo6eek3l0h9hf0a7i06a1otjpsb05d188kslln8h0jtsds&domain=1'
+    url = f'https://api.keepa.com/topseller?key=<Your Keepa API Key>&domain=1'
     req = requests.get(url)
     with open(f'topseller.json', 'wb') as f:
         f.write(req.content)
